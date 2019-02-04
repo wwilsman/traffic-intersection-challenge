689da25b36438f969a98a5ae5bbef79778376c09
Wil Wilsman <hello@wilwilsman.com>
2019-02-03 19:26:53 -0600

    Stop writing SVG by hand
    Once major adjustments needed to be made, hand-rolled SVG became
    extremely difficult to update. Better to let vector editing programs
    handle editing vectors and clean up output.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import Snap from 'snapsvg';
+import svg from './intersection.svg';
+
 
 class TrafficIntersection {
+  static load() {
+    if (this.loaded) return;
+    let root = document.querySelector('#root');
+    root.innerHTML = svg;
+    root.firstChild.id = 'intersection';
+    this.loaded = true;
+  }
+
   static start() {
+    if (!this.loaded) this.load();
     this.instance = this.instance || new TrafficIntersection();
     return this.instance;
   }
